65ab481c784d03b2cf8c71227ad0554fa1baede5
Mikhail Katsman <kry3er@gmail.com>
2023-05-11 20:10:53 +0100

    regexp1

---
+++ scripts/regexp/checkstrings.ts
@@ -0,0 +1,19 @@
+export function hey(message: string): string {
+    const m = message.trim()
+  
+    const lettersRegExp: RegExp = /[a-zA-Z]/
+    
+    if (m.length === 0) {
+      return "Fine. Be that way!"
+    }
+    if (m[m.length - 1] === "?") {
+      if (m === m.toUpperCase() && lettersRegExp.test(m)) {
+        return "Calm down, I know what I'm doing!"
+      }
+      return "Sure."
+    }
+    if (m === m.toUpperCase() && lettersRegExp.test(m)) {
+      return "Whoa, chill out!"
+    }
+    return "Whatever."
+  }
